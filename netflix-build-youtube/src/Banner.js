874af407ebe1c60c8dc74819973c55b41e6db088
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-02-19 17:36:39 -0800

    update with movie rendering

--- netflix-build-youtube/src/Banner.js
+++ netflix-build-youtube/src/Banner.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
+import axios from './axios';
+import requests from './Requests';
 
 function Banner () {
+
+    const [movie, setMovie]= useState( [] );
+
+    useEffect( () => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length -1)
+                ]
+            )
+            return request
+        }
+
+        fetchData();
+    }, [] )
+
+    console.log(movie);
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+
+
     return (
         <header className = 'banner' style={{
             backgroundSize: 'cover',
             // backgroundImage: `url("https://pixy.org/src/49/491517.png")`,
-            backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg")`,
+            // backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
             backgroundPosition: "center center",
         }}>
             <div className = "banner_contents">
                 <h1 className = "banner_title">
-                    Movie Name
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className = "banner_buttons">
                     <button className = "banner_button"> Play </button>
                     <button className = "banner_button"> My List </button>
                     <h1 className = "banner_description"> 
-                        This is a text banner_description
+                       {truncate(movie?.overview, 150)}
                     </h1>
                 </div>
             </div>
-            <div 
-                className = "banner--fadeBottom" 
-            />
+            <div className = "banner--fadeBottom"/>
         </header>
     )
 }
--- netflix-build-youtube/src/HomeScreen.js
+++ netflix-build-youtube/src/HomeScreen.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "./HomeScreen.css";
 import Nav from './Nav';
 import Banner from './Banner';
+import Row from './Row';
+import requests from "./Requests";
+
 
 function HomeScreen() {
     return (
@@ -10,8 +13,19 @@ function HomeScreen() {
 
             <Banner />
 
-            {/* Row */}
-
+            <Row 
+                title='NETFLIX ORIGINALS'
+                fetchURL={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+            <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+            <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+            <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+            <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+            <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+            <Row title="Documentaries" fetchURL={requests.fetchTrending}/>
+            <Row title="Trending Now" fetchURL={requests.fetchDocumentaries}/>
 
         </div>
     )
